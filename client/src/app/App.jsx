c321c582c122d07a777387338521d1556ccb9ff8
MashaShamash <mariapanasenko26@yandex.ru>
2024-06-21 10:19:48 +0300

    done

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,50 +1,56 @@
-import { useState } from 'react'
-import './App.css'
-import requestAxios, { setAccessToken } from '../services/axios'
-import { Route, Routes } from 'react-router-dom'
-import Main from '../page/main/Main'
-import Cards from '../page/cards/Cards'
-import Registration from '../page/auth/Registration'
-import Authorization from '../page/auth/Authorizetion'
-import Navbar from '../page/navbar/Navbar'
+import { useState } from "react";
+import "./App.css";
+import requestAxios, { setAccessToken } from "../services/axios";
+import { Route, Routes } from "react-router-dom";
+import Main from "../page/main/Main";
+import Cards from "../page/cards/Cards";
+import Registration from "../page/auth/Registration";
+import Authorization from "../page/auth/Authorization";
+import Navbar from "../page/navbar/Navbar";
 
 function App() {
-  const [user, setUser] = useState(undefined)
-  const [cards, setCards] = useState([])
+  const [user, setUser] = useState(undefined);
+  const [cards, setCards] = useState([]);
 
   const axiosCards = async () => {
-    const {data} = await requestAxios.get('/cards')
-    if(data.message === 'success'){
-      setCards(data.cards)
+    console.log("asd");
+    const { data } = await requestAxios.get("/cards");
+    console.log(data);
+    if (data.message === "success") {
+      setCards(data.cards);
     }
 
     const axiosCheckUser = async () => {
-      const { data } = await requestAxios.get('/tokens/refresh');
-      if (data.message === 'success') {
+      const { data } = await requestAxios.get("/tokens/refresh");
+      if (data.message === "success") {
         setUser(data.user);
         setAccessToken(data.accessToken);
       }
-    }
+    };
 
     useEffect(() => {
-      axiosCards()
-      axiosCheckUser()
-    },[])
-  }
+      axiosCards();
+      axiosCheckUser();
+    }, []);
+    console.log(cards);
+  };
   return (
     <>
-   <div>
-    <Navbar user={user} setUser={setUser}/>
-    <Routes>
-      <Route path='/' element={<Main/>}/>
-        {/* <Route path='/cards'element={<Cards/>}/> */}
-        <Route path='/registration' element={<Registration/>}/> 
-        <Route path='/authorization' element={<Authorization/>}/> 
-        <Route path='*' element={<h1>404</h1>} />
-    </Routes>
-   </div>
+      <div>
+        <Navbar user={user} setUser={setUser} />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route
+            path="/cards"
+            element={<Cards cards={cards} setCards={setCards} />}
+          />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/authorization" element={<Authorization />} />
+          <Route path="*" element={<h1>404</h1>} />
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/page/cards/CardItem.jsx
+++ client/src/page/cards/CardItem.jsx
@@ -1,7 +1,20 @@
-import React from'react';
-function CardItem({ title }) {
+import React from "react";
+function CardItem({ card, setCards }) {
+  const onHandleDelete = async () => {
+    const { data } = await request.delete(`/cards/${card.id}`);
+    if (data.message === "success") {
+      setCards((prev) => prev.filter((delCard) => delCard.id !== card.id));
+    }
+  };
   return (
-      <div><h1>CardItem</h1></div>
+    <div key={card.id}>
+      <img src={card.img} alt="foto" />
+      <h3>{card.name}</h3>
+      <p>{card.price}</p>
+      <p>{card.city}</p>
+      <Link to={`/movies/${movie.id}`}>Подробнее</Link>
+      <button onClick={onHandleDelete}>Удалить</button>
+    </div>
   );
 }
 
